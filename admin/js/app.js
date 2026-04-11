2931668e4b99f08ff3c80c4cc68f398e1103979b
Himanshu pal <himanshupalsingh6@gmail.com>
2026-04-11 21:33:05 +0530

    Create app.js

---
+++ admin/js/app.js
@@ -0,0 +1,21 @@
+import { loadDashboard } from "./dashboard.js";
+import { loadOrders } from "./orders.js";
+import { loadUsers } from "./users.js";
+
+window.showTab = function(tab){
+
+document.querySelectorAll(".tab").forEach(t=>t.classList.remove("active"));
+
+document.querySelectorAll(".tab").forEach(t=>{
+  if(t.innerText.toLowerCase().includes(tab)){
+    t.classList.add("active");
+  }
+});
+
+if(tab==="dashboard") loadDashboard();
+if(tab==="orders") loadOrders();
+if(tab==="users") loadUsers();
+};
+
+// default
+loadDashboard();
