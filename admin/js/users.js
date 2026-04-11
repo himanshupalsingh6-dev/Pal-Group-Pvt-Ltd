83e046a9afd7bdde67ff47e315735aa746423ccd
Himanshu pal <himanshupalsingh6@gmail.com>
2026-04-11 21:41:44 +0530

    Update users.js

--- admin/js/users.js
+++ admin/js/users.js
@@ -1,13 +1,13 @@
 import { db } from "../firebase.js";
 import {
-collection,onSnapshot,query,where,getDocs
+collection,onSnapshot
 } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 let users=[];
 
 onSnapshot(collection(db,"users"), snap=>{
 users=[];
-snap.forEach(d=>users.push({...d.data(),id:d.id}));
+snap.forEach(d=>users.push(d.data()));
 });
 
 export function loadUsers(){
@@ -19,22 +19,8 @@ html+=`
 <div class="card">
 ${u.email}<br>
 ${u.phone||""}
-<button onclick="view('${u.id}')">Orders</button>
 </div>
 `;
-});
-
-document.getElementById("content").innerHTML=html;
-}
-
-window.view=async(uid)=>{
-let q=query(collection(db,"orders"),where("userId","==",uid));
-let snap=await getDocs(q);
-
-let html="";
-snap.forEach(d=>{
-let o=d.data();
-html+=`<div class="card">${o.name} ₹${o.total}</div>`;
 });
 
 document.getElementById("content").innerHTML=html;
