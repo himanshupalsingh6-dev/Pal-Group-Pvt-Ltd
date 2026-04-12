b9b3bd83bb0f3a7080d5379696633d7ccf8466e9
Himanshu pal <himanshupalsingh6@gmail.com>
2026-04-12 15:54:04 +0530

    Create common.js

---
+++ partner/js/common.js
@@ -0,0 +1,14 @@
+export function sidebar(){
+return `
+<div class="sidebar">
+<h2>⚡ QuickPress</h2>
+
+<a href="index.html">📊 Dashboard</a>
+<a href="orders.html">📦 Orders</a>
+<a href="history.html">📜 History</a>
+<a href="analytics.html">📈 Analytics</a>
+<a href="outlet.html">🏪 Outlet</a>
+
+</div>
+`;
+}
