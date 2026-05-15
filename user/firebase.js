9b626a764beebacdf2bb21e5311ab62d0c2de6b2
Himanshu pal <himanshupalsingh6@gmail.com>
2026-05-15 13:49:19 +0530

    Update firebase.js

--- user/firebase.js
+++ user/firebase.js
@@ -1,17 +1,31 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+import {
+getAuth
+} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBaslH5QsahoyESYpo1nVtlYIUYfVJ2d4",
-  authDomain: "quickpress-offical.firebaseapp.com",
-  projectId: "quickpress-offical",
-  storageBucket: "quickpress-offical.firebasestorage.app",
-  messagingSenderId: "939286557961",
-  appId: "1:939286557961:web:ddac64f197e14877718fd8"
+
+apiKey: "AIzaSyBBaslH5QsahoyESYpo1nVtlYIUYfVJ2d4",
+
+authDomain: "quickpress-offical.firebaseapp.com",
+
+projectId: "quickpress-offical",
+
+storageBucket: "quickpress-offical.firebasestorage.app",
+
+messagingSenderId: "939286557961",
+
+appId: "1:939286557961:web:ddac64f197e14877718fd8"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export const auth = getAuth(app);
