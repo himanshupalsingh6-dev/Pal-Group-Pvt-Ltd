ef9e74e489dea0f0db81ede0d634bd35687a7aa3
Himanshu pal <himanshupalsingh6@gmail.com>
2026-05-16 21:14:33 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,103 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+/* =========================================================
+FILE : firebase.js
+========================================================= */
+
+/* =========================================================
+FIREBASE IMPORT
+========================================================= */
+
+import { initializeApp }
+
+from
+
+"https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 
 import {
+
 getFirestore
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 import {
+
 getAuth
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+
+import {
+
+getStorage
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/12.11.0/firebase-storage.js";
+
+/* =========================================================
+FIREBASE CONFIG
+========================================================= */
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyBBaslH5QsahoyESYpo1nVtlYIUYfVJ2d4",
+apiKey:
+"AIzaSyBBaslH5QsahoyESYpo1nVtlYIUYfVJ2d4",
 
-authDomain: "quickpress-offical.firebaseapp.com",
+authDomain:
+"quickpress-offical.firebaseapp.com",
 
-projectId: "quickpress-offical",
+projectId:
+"quickpress-offical",
 
-storageBucket: "quickpress-offical.firebasestorage.app",
+storageBucket:
+"quickpress-offical.firebasestorage.app",
 
-messagingSenderId: "939286557961",
+messagingSenderId:
+"939286557961",
 
-appId: "1:939286557961:web:ddac64f197e14877718fd8"
+appId:
+"1:939286557961:web:ddac64f197e14877718fd8"
 
 };
 
-const app = initializeApp(firebaseConfig);
+/* =========================================================
+INITIALIZE FIREBASE
+========================================================= */
 
-export const db = getFirestore(app);
+const app =
+initializeApp(
+firebaseConfig
+);
 
-export const auth = getAuth(app);
+/* =========================================================
+SERVICES
+========================================================= */
+
+const db =
+getFirestore(app);
+
+const auth =
+getAuth(app);
+
+const storage =
+getStorage(app);
+
+/* =========================================================
+EXPORT
+========================================================= */
+
+export {
+
+db,
+auth,
+storage
+
+};
