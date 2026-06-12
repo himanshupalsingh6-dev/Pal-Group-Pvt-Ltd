230d40e3fb22d8c0b2a25e7ea05451232381df27
Himanshu Baghel  💗 <himanshupalsingh6@gmail.com>
2026-06-12 13:27:51 +0530

    Update partners.js

--- admin/partners.js
+++ admin/partners.js
@@ -22,9 +22,7 @@ serverTimestamp
 }
 
 from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
-========================================== */
-ADMIN AUTH CHECK
-========================================== */
+
 
 if(
 
